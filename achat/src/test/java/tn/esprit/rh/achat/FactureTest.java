86c3ec153fa0a62cc9d715eaa68ab0a99b01febf
YounesJemli <162110460+YounesJemli@users.noreply.github.com>
2024-05-07 16:16:08 +0100

    Update FactureTest.java

--- achat/src/test/java/tn/esprit/rh/achat/FactureTest.java
+++ achat/src/test/java/tn/esprit/rh/achat/FactureTest.java
@@ -1,9 +1,15 @@
 package tn.esprit.rh.achat;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.rh.achat.entities.Facture;
+import tn.esprit.rh.achat.repositories.FactureRepository;
+import tn.esprit.rh.achat.services.FactureServiceImpl;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class FactureTest {
     @Mock
     private FactureRepository factureRepository;
