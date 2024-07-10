a64b22e4d06118210e92ea90072f2bbe76fd7d1b
YounesJemli <162110460+YounesJemli@users.noreply.github.com>
2024-07-10 13:27:11 +0100

    Update jest.config.js

--- crud-tuto-front/jest.config.js
+++ crud-tuto-front/jest.config.js
@@ -1,21 +1,18 @@
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/src/setupJest.ts'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.spec.json',
       stringifyContentPathRegex: '\\.html$',
-      astTransformers: {
-        before: [
-          'jest-preset-angular/build/InlineFilesTransformer',
-          'jest-preset-angular/build/StripStylesTransformer',
-        ],
-      },
     },
   },
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(ts|js|html)$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
 };
