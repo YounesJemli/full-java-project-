68c8cad75f907052f8af75d8bbf2707465ba0479
YounesJemli <162110460+YounesJemli@users.noreply.github.com>
2024-07-22 15:41:02 +0100

    Delete kubernetes-terra directory

--- kubernetes-terra/secret-manager/output.tf
+++
@@ -1,9 +0,0 @@
-output "secret_id" {
-  description = "The ID of the created secret"
-  value       = google_secret_manager_secret.my_secret.id
-}
-
-output "secret_version" {
-  description = "The version of the created secret"
-  value       = google_secret_manager_secret_version.my_secret_version.id
-}
--- kubernetes-terra/secret-manager/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-project_id       = "lofty-shine-427016-t2"
-region           = "europe-west1-d"
-secret_id        = "service-account-key"
-secret_file_path = "~/Desktop/lofty-shine-427016-t2-587aaaabf199.json"
