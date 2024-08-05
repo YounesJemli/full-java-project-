5e3f7405882a2b5554288e862808069da21e42f8
YounesJemli <162110460+YounesJemli@users.noreply.github.com>
2024-08-05 12:48:46 +0100

    Update AchatApplication.java

--- achat/src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ achat/src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -3,13 +3,24 @@ package tn.esprit.rh.achat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+import tn.esprit.rh.achat.entities.Produit;
+import tn.esprit.rh.achat.services.IProduitService;
 @EnableScheduling
 @SpringBootApplication
 public class AchatApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AchatApplication.class, args);
+        List<Produit> produits = produitService.retrieveAllProduits();
+            
+            // Afficher les produits dans la console
+            if (produits.isEmpty()) {
+                System.out.println("Aucun produit trouvé.");
+            } else {
+                for (Produit produit : produits) {
+                    System.out.println("Produit : " + produit);
+                }
+            }
     }
 
 }
