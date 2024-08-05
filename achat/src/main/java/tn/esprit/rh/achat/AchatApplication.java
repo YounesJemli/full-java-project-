b9550fff0f6cd07696a544362e96f637b5aa80b8
YounesJemli <162110460+YounesJemli@users.noreply.github.com>
2024-08-05 12:55:08 +0100

    Update AchatApplication.java

--- achat/src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ achat/src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -1,18 +1,33 @@
-package tn.esprit.rh.achat;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
 import tn.esprit.rh.achat.entities.Produit;
 import tn.esprit.rh.achat.services.IProduitService;
-@EnableScheduling
+
+import java.util.List;
+
 @SpringBootApplication
 public class AchatApplication {
 
+    private final IProduitService produitService;
+
+    @Autowired
+    public AchatApplication(IProduitService produitService) {
+        this.produitService = produitService;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AchatApplication.class, args);
-        List<Produit> produits = produitService.retrieveAllProduits();
-            
+    }
+
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+            // Récupérer tous les produits
+            List<Produit> produits = produitService.retrieveAllProduits();
+
             // Afficher les produits dans la console
             if (produits.isEmpty()) {
                 System.out.println("Aucun produit trouvé.");
@@ -21,6 +36,6 @@ public class AchatApplication {
                     System.out.println("Produit : " + produit);
                 }
             }
+        };
     }
-
 }
